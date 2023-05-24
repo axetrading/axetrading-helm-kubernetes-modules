7cda58042e90981c62f755b43c82b46d1bdc14be
Ciprian <cursu@axetrading.com>
2023-05-24 12:30:44 +0300

    updating the role with the policy for autoscaling

--- modules/cluster-autoscaler/iam-policies.tf
+++ modules/cluster-autoscaler/iam-policies.tf
@@ -27,3 +27,21 @@ data "aws_iam_policy_document" "autoscaler_policy" {
     resources = ["*"]
   }
 }
+
+resource "aws_iam_policy" "cluster_autoscaler" {
+  count = var.create_role ? 1 : 0
+
+  name_prefix = "eks-cluster-autoscaler-"
+  path        = var.role_path
+  description = "Policy for cluster autoscaler"
+  policy      = data.aws_iam_policy_document.cluster_autoscaler[0].json
+
+  tags = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_autoscaler" {
+  count = var.create_role ? 1 : 0
+
+  role       = aws_iam_role.this[0].name
+  policy_arn = aws_iam_policy.cluster_autoscaler[0].arn
+}
