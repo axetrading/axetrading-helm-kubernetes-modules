e6996ea1eb61f52df2658ee480bb91feedde4d42
Ciprian <cursu@axetrading.com>
2023-11-16 10:06:20 +0200

    changing variable type to string for scrape timeout and interval

--- modules/kube-prometheus-stack/variables.tf
+++ modules/kube-prometheus-stack/variables.tf
@@ -259,13 +259,13 @@ variable "alertmanager_external_url" {
 }
 
 variable "prometheus_scrape_interval" {
-  type        = number
+  type        = string
   description = "The Prometheus scrape interval in seconds"
-  default     = 30
+  default     = "30"
 }
 
 variable "prometheus_scrape_timeout" {
-  type        = number
+  type        = string
   description = "The Prometheus scrape timeout interval in seconds"
-  default     = 10
+  default     = "10"
 }
